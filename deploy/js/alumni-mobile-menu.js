d2640e0601d041b337cdd253c96389e7318b2460
Dave Bayles <dbayles@carnegiehighered.com>
2024-05-28 17:03:10 -0500

    Update functionality based on feedback.

--- deploy/js/alumni-mobile-menu.js
+++ deploy/js/alumni-mobile-menu.js
@@ -1,19 +1,57 @@
-  // header
-  const headers = document.querySelectorAll('.Header');
+const stickyTargets = document.querySelectorAll('[data-sticky]');
+const stickyTargetScroll = document.querySelectorAll('[data-stuck]');
 
-  const observer = new IntersectionObserver((entries) => {
-    for (const entry of entries) {
-      const { intersectionRatio, target } = entry;
-      target.toggleAttribute('data-stuck', intersectionRatio < 1);
+stickyTargets.forEach((target) => {
+  // Add a scroll event listener to the window
+  window.addEventListener('scroll', function() {
+    // Get the current scroll position
+    const scrollPosition = window.scrollY || window.pageYOffset;
+
+    // Check if the scroll position is greater than or equal to 100 pixels
+    if (scrollPosition >= 400) {
+      // Add the data-sticky attribute to the target div
+      target.setAttribute('data-sticky', 'true');
+    } else {
+      // If the scroll position is less than 100 pixels, remove the data-sticky attribute
+      target.removeAttribute('data-sticky');
     }
-  }, {
-    rootMargin: '156px 0px 0px 0px',
-    threshold: [1],
   });
+});
 
-  for (const header of headers) {
-    observer.observe(header);
-  };
+stickyTargetScroll.forEach((target) => {
+  // Add a scroll event listener to the window
+  window.addEventListener('scroll', function() {
+    // Get the current scroll position
+    const scrollPosition = window.scrollY || window.pageYOffset;
+
+    // Check if the scroll position is greater than or equal to 100 pixels
+    if (scrollPosition >= 650) {
+      // Add the data-sticky attribute to the target div
+      target.setAttribute('data-stuck', 'true');
+    } else {
+      // If the scroll position is less than 100 pixels, remove the data-sticky attribute
+      target.removeAttribute('data-stuck');
+    }
+  });
+});
+
+const root = document.documentElement;
+let last = 0;
+
+const updatee = () => {
+  const position = root.getBoundingClientRect().top;
+  if (Math.abs(last - position) > 48) {
+    if (position > last) {
+      root.setAttribute('data-scroll', 'up');
+    } else {
+      root.setAttribute('data-scroll', 'down');
+    }
+    last = position;
+  }
+}
+
+window.addEventListener('load', updatee);
+window.addEventListener('scroll', updatee);
 
   // search toggle
   const searchToggles = document.querySelectorAll('.Header__search-toggle');
