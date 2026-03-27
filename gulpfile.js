2242f9da619172bd08aacc6102aed7db0f848b07
Dave Bayles <dbayles@carnegiehighered.com>
2026-03-27 14:17:03 -0400

    Add secondary page override CSS bundles, fix breadcrumbs and blockquote
    - Add luc-secondlevelpage.css, luc-adjust-carnegie-global.css, and
      luc-subsites.css SCSS entry points and gulp tasks for interior page overrides
    - Scope breadcrumb cream background to non-elevated pages so framework's
      :has(+ .lead-panel-two-column-left-right) styles work on elevated pages
    - Fix blockquote image overlay clipping by overriding overflow:hidden
      when using container-wrap layout
    - Update secondary page HTML structure: add main-content-wrapper-full-width,
      bg-white on blockquote and LGS button wrappers, match T4 panel structure
      with padding dividers and container classes
    - Add CI build support for new CSS bundles

--- gulpfile.js
+++ gulpfile.js
@@ -105,6 +105,33 @@ function sassComponents() {
     .pipe(gulp.dest(config.deployDir + '/css'));
 };
 
+function sassSecondLevelPage() {
+    return gulp
+    .src(config.devDir + '/scss/scss-nav-footer/luc-secondlevelpage.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.deployDir + '/css'));
+};
+
+function sassAdjustCarnegieGlobal() {
+    return gulp
+    .src(config.devDir + '/scss/scss-nav-footer/luc-adjust-carnegie-global.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.deployDir + '/css'));
+};
+
+function sassSubsites() {
+    return gulp
+    .src(config.devDir + '/scss/scss-nav-footer/luc-subsites.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.deployDir + '/css'));
+};
+
 // -----------------------------------------------------------------------------
 
 function referencePaths() {
@@ -143,13 +170,13 @@ function watchFiles() {
     }
   });
   watch('./development/**/*.html', gulp.series('indexBuild', 'browserSyncReload'));
-  watch(config.devDir + '/scss/**/*.scss', gulp.series(parallel('sassFramework', 'sassGrid', 'sassTypography', 'sassPanels', 'sassPanelsDev', 'sassDocumentation', 'sassNavFooter', 'sassComponents'), 'browserSyncReload'));
+  watch(config.devDir + '/scss/**/*.scss', gulp.series(parallel('sassFramework', 'sassGrid', 'sassTypography', 'sassPanels', 'sassPanelsDev', 'sassDocumentation', 'sassNavFooter', 'sassComponents', 'sassSecondLevelPage', 'sassAdjustCarnegieGlobal', 'sassSubsites'), 'browserSyncReload'));
   watch(config.devDir + '/js/**/*.js', gulp.series('browserSyncReload'));
 };
 
 // Default task: build + watch + live reload
 exports.default = series(
-  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation, sassNavFooter, sassComponents),
+  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation, sassNavFooter, sassComponents, sassSecondLevelPage, sassAdjustCarnegieGlobal, sassSubsites),
   referencePaths,
   indexBuild,
   watchFiles,
@@ -158,7 +185,7 @@ exports.default = series(
 
 // CI build task: compile everything without starting a dev server
 exports.build = series(
-  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation, sassNavFooter, sassComponents),
+  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation, sassNavFooter, sassComponents, sassSecondLevelPage, sassAdjustCarnegieGlobal, sassSubsites),
   referencePaths,
   indexBuild
 );
@@ -173,6 +200,9 @@ exports.sassPanelsDev = sassPanelsDev;
 exports.sassDocumentation = sassDocumentation;
 exports.sassNavFooter = sassNavFooter;
 exports.sassComponents = sassComponents;
+exports.sassSecondLevelPage = sassSecondLevelPage;
+exports.sassAdjustCarnegieGlobal = sassAdjustCarnegieGlobal;
+exports.sassSubsites = sassSubsites;
 exports.watchFiles = watchFiles;
 exports.browserSync = browserSync;
 exports.browserSyncReload = browserSyncReload;
