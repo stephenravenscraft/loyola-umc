ce5f649da26eb40fc7e24d137174a507c9d06075
Dave Bayles <dbayles@carnegiehighered.com>
2026-03-27 00:28:30 -0400

    Restore separate luc-nav-footer.css and luc-components.css bundles, clean up SCSS
    - Restore scss-nav-footer/ directory with entry points for luc-nav-footer.css
      and luc-components.css, matching live site's CSS architecture
    - Add sassNavFooter and sassComponents gulp tasks, CI build task, and watch tasks
    - Exclude luc-*.css from gulp-inject; hardcode after endinject in all HTML templates
      so they load last in the cascade (matching production behavior)
    - Remove 239 lines of commented-out legacy header/footer styles from _framework.scss
    - Clean up commented-out imports in styles-framework.scss
    - Fix duplicate variables in _custom-variables.scss ($luc-transition-fast,
      $copper-extra-light, $luc-shadow-heavy)
    - Clean up _typography.scss formatting
    - Add gulp build step to bitbucket-pipelines.yml for CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,16 @@
 //
 const browserSync = require('browser-sync').create();
-// const requireDir = require('require-dir');
-
 
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
-// const jshint = require('gulp-jshint');
 const fileinclude = require('gulp-file-include');
 const useref = require('gulp-useref');
 const sourcemaps = require('gulp-sourcemaps');
 const inject = require('gulp-inject');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
-// const postcss = require('gulp-postcss')
 const concat = require('gulp-concat');
-// const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
-// const cssnano = require('gulp-cssnano');
-// const gulpSequence = require('gulp-sequence');
 
 const config = {
   devDir: './development',
@@ -31,113 +24,111 @@ function htmlInclude() {
     return gulp.src(['./development/*.html'])
     .pipe(fileinclude())
     .pipe(gulp.dest('./deploy'));
-    cb();
 };
 
 function sassFramework() {
     return gulp
     .src(config.devDir + '/scss/scss-framework/**/*.scss')
-    .pipe(sourcemaps.init())  // Process the original sources
-		.pipe(sass())
-		.pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.deployDir + '/css'));
-    cb();
 };
 
 function sassGrid() {
     return gulp
     .src(config.devDir + '/scss/scss-grid/**/*.scss')
-    .pipe(sourcemaps.init())  // Process the original sources
-		.pipe(sass())
-		.pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.deployDir + '/css'));
-    cb();
 };
 
 function sassTypography() {
     return gulp
     .src(config.devDir + '/scss/scss-typography/**/*.scss')
-    .pipe(sourcemaps.init())  // Process the original sources
-		.pipe(sass())
-		.pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.deployDir + '/css'));
-    cb();
 };
 
 function sassPanels() {
     return gulp
     .src(config.devDir + '/scss/scss-panels/**/*.scss')
-    .pipe(sourcemaps.init())  // Process the original sources
-		.pipe(sass())
-		.pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.deployDir + '/css'));
-    cb();
 };
 
 function sassPanelsDev() {
-  return gulp
-  .src(config.devDir + '/scss/scss-panels-dev/**/*.scss')
-  .pipe(sourcemaps.init())  // Process the original sources
-  .pipe(sass())
-  .pipe(sourcemaps.write()) // Add the map to modified source.
-  .pipe(gulp.dest(config.deployDir + '/css'));
-  cb();
+    return gulp
+    .src(config.devDir + '/scss/scss-panels-dev/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.deployDir + '/css'));
 };
-
-// function sassCarnegieComponents() {
-//   return gulp
-//   .src(config.devDir + '/scss/scss-carnegie-components/**/*.scss')
-//   .pipe(sourcemaps.init())  // Process the original sources
-//   .pipe(sass())
-//   .pipe(sourcemaps.write()) // Add the map to modified source.
-//   .pipe(gulp.dest(config.deployDir + '/css'));
-//   cb();
-// };
 
 function sassDocumentation() {
     return gulp
     .src(config.devDir + '/scss/scss-documentation/**/*.scss')
-    .pipe(sourcemaps.init())  // Process the original sources
-		.pipe(sass())
-		.pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.deployDir + '/css'));
-    cb();
 };
+
+// -----------------------------------------------------------------------------
+// Nav/Footer and Components bundles
+// These compile to the standalone CSS files loaded on the live site:
+//   luc-nav-footer.css  — header, footer, utility nav
+//   luc-components.css  — 2026 homepage components
+// -----------------------------------------------------------------------------
+
+function sassNavFooter() {
+    return gulp
+    .src(config.devDir + '/scss/scss-nav-footer/luc-nav-footer.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.deployDir + '/css'));
+};
+
+function sassComponents() {
+    return gulp
+    .src(config.devDir + '/scss/scss-nav-footer/luc-components.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.deployDir + '/css'));
+};
+
+// -----------------------------------------------------------------------------
 
 function referencePaths() {
     return gulp
     .src('./development/*.html')
     .pipe(useref())
-		.pipe(gulp.dest(config.deployDir));
-    cb();
+    .pipe(gulp.dest(config.deployDir));
 };
-
-// function indexBuild() {
-//   var target = gulp.src('./development/*.html');
-//   var sources = gulp.src(['./deploy/js/**/*.js', './deploy/css/**/*.css'], {read: false});
-//  	return target.pipe(inject(sources, {ignorePath: 'deploy/', addRootSlash: false}))
-//  	.pipe(gulpIf('*.html', fileinclude({prefix: '@@', basepath: '@file'})))
-// 	.pipe(gulp.dest(config.deployDir));
-//   cb();
-// };
 
 function indexBuild(cb) {
   var target = gulp.src('./development/*.html');
 
   var jsSources = gulp.src(['./deploy/js/**/*.js', '!./deploy/js/bundle.js'])
-    .pipe(concat('bundle.js')) // concatenate all JS files into bundle.js
-    .pipe(uglify()) // minify bundle.js
-    .pipe(gulp.dest('./deploy/js')); // Save the bundle.js file in the ./deploy/js directory
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./deploy/js'));
 
-  var cssSources = gulp.src('./deploy/css/**/*.css');
+  var cssSources = gulp.src(['./deploy/css/**/*.css', '!./deploy/css/luc-*.css'], { read: false });
 
   return target
     .pipe(inject(jsSources, { ignorePath: 'deploy/', addRootSlash: false }))
     .pipe(inject(cssSources, { ignorePath: 'deploy/', addRootSlash: false }))
     .pipe(gulpIf('*.html', fileinclude({ prefix: '@@', basepath: '@file' })))
     .pipe(gulp.dest(config.deployDir));
-
-  cb();
 }
 
 function browserSyncReload(cb) {
@@ -152,17 +143,26 @@ function watchFiles() {
     }
   });
   watch('./development/**/*.html', gulp.series('indexBuild', 'browserSyncReload'));
-  watch(config.devDir + '/scss/**/*.scss', gulp.series(parallel('sassFramework', 'sassGrid', 'sassTypography', 'sassPanels', 'sassPanelsDev', 'sassDocumentation'), 'browserSyncReload'));
+  watch(config.devDir + '/scss/**/*.scss', gulp.series(parallel('sassFramework', 'sassGrid', 'sassTypography', 'sassPanels', 'sassPanelsDev', 'sassDocumentation', 'sassNavFooter', 'sassComponents'), 'browserSyncReload'));
   watch(config.devDir + '/js/**/*.js', gulp.series('browserSyncReload'));
 };
 
+// Default task: build + watch + live reload
 exports.default = series(
-  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation),
+  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation, sassNavFooter, sassComponents),
   referencePaths,
   indexBuild,
   watchFiles,
   browserSyncReload
 );
+
+// CI build task: compile everything without starting a dev server
+exports.build = series(
+  parallel(sassFramework, sassGrid, sassTypography, sassPanels, sassPanelsDev, sassDocumentation, sassNavFooter, sassComponents),
+  referencePaths,
+  indexBuild
+);
+
 exports.indexBuild = indexBuild;
 exports.referencePaths = referencePaths;
 exports.sassFramework = sassFramework;
@@ -170,8 +170,9 @@ exports.sassGrid = sassGrid;
 exports.sassTypography = sassTypography;
 exports.sassPanels = sassPanels;
 exports.sassPanelsDev = sassPanelsDev;
-// exports.sassCarnegieComponents = sassCarnegieComponents;
 exports.sassDocumentation = sassDocumentation;
+exports.sassNavFooter = sassNavFooter;
+exports.sassComponents = sassComponents;
 exports.watchFiles = watchFiles;
 exports.browserSync = browserSync;
 exports.browserSyncReload = browserSyncReload;
